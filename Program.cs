c42bd2bf8cb735c56d68949c57f64c4be4507e71
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-02 11:26:12 +0300

    SSH commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Random change");
+            Console.WriteLine("Random change????????");
         }
     }
 }
