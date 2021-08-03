7de72f9dd48824c97bea959deb1fbe28106bf893
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-03 10:32:31 +0300

    Created Intex.js

---
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#addMemberButtonId").click(function () {
+        var newcomerName = $("#nameInputId").val();
+
+        // Remember string interpolation
+        $("#teamMembersList").append(`<li>${newcomerName}</li>`);
+
+        $("#nameInputId").val("");
+    })
+});
