244fbe60ba41a6e3314481f358fc91e4ae82aec6
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-05 14:52:04 +0300

    Detele tests, getMemberById tests

--- HelloWorldWeb.Tests/UnitTest1.cs
+++ HelloWorldWeb.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using HelloWorldWeb.Models;
 using HelloWorldWeb.Services;
 using System;
 using Xunit;
@@ -20,6 +21,96 @@ namespace HelloWorldWeb.Tests
 
             // Assert
             Assert.Equal(7, teamService.GetTeamInfo().TeamMembers.Count);
+        }
+
+        [Fact]
+        public void GetItemByIdNotNull()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            int existingId= teamService.GetTeamInfo().TeamMembers[0].Id;
+
+            // Act
+            TeamMember member = teamService.GetTeamMemberById(existingId);
+
+            // Assert
+            Assert.NotNull(member);
+        }
+
+        [Fact]
+        public void GetItemByIdNull()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            int givenId = 1010101;
+
+            // Act
+            TeamMember member = teamService.GetTeamMemberById(givenId);
+
+            // Assert
+            Assert.Null(member);
+        }
+
+        [Fact]
+        public void GetItemByIdFound()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            int givenId = 100;
+
+            // Act
+            teamService.AddTeamMember(new TeamMember(givenId, "Cat"));
+            TeamMember member = teamService.GetTeamMemberById(givenId);
+
+            // Assert
+            Assert.True(member.Equals(new TeamMember(givenId, "Cat")));
+        }
+
+        [Fact]
+        public void DeleteDefaultMemberByIdTest()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            int idCounter = TeamMember.GetIdCounter();
+
+            // Act
+            teamService.DeleteTeamMember(idCounter);
+
+            // Assert
+            Assert.Null(teamService.GetTeamMemberById(idCounter));
+        }
+
+        [Fact]
+        public void DeleteNewMemberByStaticIdTest()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+
+
+            // Act
+            TeamMember newTeamMember = new TeamMember("Cthulhu");
+            teamService.AddTeamMember(newTeamMember);
+            teamService.DeleteTeamMember(newTeamMember.Id);
+
+            // Assert
+            Assert.Null(teamService.GetTeamMemberById(newTeamMember.Id));
+        }
+
+
+        [Fact]
+        public void DeleteNewMemberByGivenIdTest()
+        {
+            // Assume
+            ITeamService teamService = new TeamService();
+            int givenId = 2000;
+
+            // Act
+            TeamMember newTeamMember = new TeamMember(givenId, "Cthulhu");
+            teamService.AddTeamMember(newTeamMember);
+            teamService.DeleteTeamMember(givenId);
+
+            // Assert
+            Assert.Null(teamService.GetTeamMemberById(givenId));
         }
     }
 }
