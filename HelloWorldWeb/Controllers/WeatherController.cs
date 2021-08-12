4efbfbf37e4499b0e34c51a174b637a6499e41d7
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-12 11:25:33 +0300

    Made an api call to openweathermap from backend

--- HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -1,5 +1,6 @@
 ﻿using HelloWorldWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,30 @@ namespace HelloWorldWeb.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        private readonly string latitude = "46.7700";
+        private readonly string longitude = "23.5800";
+        private readonly string apiKey = "537221f38b987a406521718bec389b6c";
+
         // GET: api/<WeatherController>
         [HttpGet]
         public IEnumerable<DailyWeatherRecord> Get()
         {
-            return new DailyWeatherRecord[] { 
+            // https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}
+            // https://api.openweathermap.org/data/2.5/onecall?lat=46.7700&lon=23.5800&exclude=hourly,minutely&appid=537221f38b987a406521718bec389b6c
+            // $ = string interpolation
+            var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            return ConvertResponseToWeatherForecastList(response.Content);
+
+           
+        }
+
+        private IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherForecastList(string content)
+        {
+            return new DailyWeatherRecord[] {
                 new DailyWeatherRecord(new DateTime(2021, 08, 12), 22.0f, WeatherType.Mild),
                 new DailyWeatherRecord(new DateTime(2021, 08, 13), 25.0f, WeatherType.Mild),
             };
@@ -30,22 +50,6 @@ namespace HelloWorldWeb.Controllers
             return "value";
         }
 
-        // POST api/<WeatherController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<WeatherController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<WeatherController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
