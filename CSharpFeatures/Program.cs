c07ed61bdfa5b69238d53a2a887013539db78038
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-19 09:43:56 +0300

    Experimented with the serialization/deserialization of an object

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,21 @@
+﻿using HelloWorldWeb.Models;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamMember teamMember = new TeamMember("Member1");
+            string jsonString = JsonSerializer.Serialize(teamMember);
+            File.WriteAllText("./teamMember.json", jsonString);
+
+            TeamMember teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(jsonString);
+            Console.WriteLine(teamMemberDeserialized);
+
+        }
+    }
+}
